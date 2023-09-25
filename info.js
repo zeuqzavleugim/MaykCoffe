282a50a29e7b2aa5db3a7cad9d016fedc16fb330
zeuqzavleugim <miguelvazquez870@gmail.com>
2023-09-25 16:58:54 -0600

    changes

--- info.js
+++ info.js
@@ -14,3 +14,100 @@ function redirigir(pagina){
             break;
     }
 }
+
+function pagina(pagina){
+    const div = document.getElementById("1");
+    const ul = document.createElement('ul');
+    const li0 = document.createElement('li');
+    const li1 = document.createElement('li');
+    const li2 = document.createElement('li');
+    const a0 = document.createElement('a');
+    const a1 = document.createElement('a');
+    const a2 = document.createElement('a');
+    switch (pagina){
+        case 'index':
+            ul.className = "nav justify-content-center nav-tabs";
+            li0.className = "nav-item";
+            li1.className = "nav-item"; 
+            li2.className = "nav-item";
+            a1.className = "nav-link text-white"; 
+            a2.className = "nav-link text-white";
+            a0.className = "nav-link text-bg-light active";
+            a0.textContent= "MaykCoffe";
+            a0.href = "index.html";
+            a1.textContent = "Quienes Somos";
+            a1.href = "info.html";
+            a2.textContent = "Menú";
+            a2.href = "menu.html";
+            li0.appendChild(a0);
+            ul.appendChild(li0);
+            li1.appendChild(a1);
+            ul.appendChild(li1);
+            li2.appendChild(a2);
+            ul.appendChild(li2);
+            div.appendChild(ul);
+            break;
+        case 'info':
+            ul.className = "nav justify-content-center nav-tabs";
+            li0.className = "nav-item";
+            li1.className = "nav-item"; 
+            li2.className = "nav-item";
+            a0.className = "nav-link text-white"; 
+            a2.className = "nav-link text-white";
+            a1.className = "nav-link text-bg-light active";
+            a0.textContent= "MaykCoffe";
+            a0.href = "index.html";
+            a1.textContent = "Quienes Somos";
+            a1.href = "info.html";
+            a2.textContent = "Menú";
+            a2.href = "menu.html";
+            li0.appendChild(a0);
+            ul.appendChild(li0);
+            li1.appendChild(a1);
+            ul.appendChild(li1);
+            li2.appendChild(a2);
+            ul.appendChild(li2);
+            div.appendChild(ul);
+            break;
+        case 'menu':
+            ul.className = "nav justify-content-center nav-tabs";
+            li0.className = "nav-item";
+            li1.className = "nav-item"; 
+            li2.className = "nav-item"; 
+            a0.className = "nav-link text-white"; 
+            a1.className = "nav-link text-white";
+            a2.className = "nav-link text-bg-light active";
+            a0.textContent= "MaykCoffe";
+            a0.href = "index.html";
+            a1.textContent = "Quienes Somos";
+            a1.href = "info.html";
+            a2.textContent = "Menú";
+            a2.href = "menu.html";
+            li0.appendChild(a0);
+            ul.appendChild(li0);
+            li1.appendChild(a1);
+            ul.appendChild(li1);
+            li2.appendChild(a2);
+            ul.appendChild(li2);
+            div.appendChild(ul);
+            break;
+    }
+}
+/*
+<div>
+    <ul class="nav justify-content-center nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link text-bg-light active" href="index.html">MaykCoffe</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="info.html">Quienes Somos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="menu.html">Menú</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="contactos.html">Contactanos</a>
+        </li>
+    </ul>
+</div>
+*/
